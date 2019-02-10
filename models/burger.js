501dd8ab014e83657637c2c442026a92a3f6510e
jley914 <jleyva914@gmail.com>
2019-02-09 22:04:02 -0700

    initial commit

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll(function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (burger, cb) {
+        orm.insertOne(burger, function (res) {
+            cb(res);
+        });
+    },
+    updateOne: function (id, cb) {
+        orm.updateOne([id], function (res) {
+            cb(res);
+        });
+    },
+    delete: function (condition, cb) {
+        orm.delete("cats", condition, function (res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
